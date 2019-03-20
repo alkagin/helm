32f76164d6896649548921448904298d5118a42c
Matt Explosion <aldiyen@gmail.com>
2019-03-19 22:44:20 -0400

    Revert "Move on with our lives"
    This reverts commit a10470695a936859ab52cfb59ff6c84a37a87a73.

--- http4s/src/test/scala/Integration.scala
+++ http4s/src/test/scala/Integration.scala
@@ -1,65 +1,65 @@
-// package helm
-// package http4s
+package helm
+package http4s
 
-// import scala.concurrent.duration.DurationInt
-// import cats.effect.IO
-// import cats.implicits._
-// import com.spotify.docker.client.{DefaultDockerClient, DockerClient}
-// import com.whisk.docker._
-// import com.whisk.docker.impl.spotify.SpotifyDockerFactory
-// import com.whisk.docker.scalatest._
-// import journal.Logger
-// import org.http4s._
-// import org.http4s.client.blaze._
-// import org.scalacheck._
-// import org.scalatest._
-// import org.scalatest.enablers.CheckerAsserting
-// import org.scalatest.prop._
+import scala.concurrent.duration.DurationInt
+import cats.effect.IO
+import cats.implicits._
+import com.spotify.docker.client.{DefaultDockerClient, DockerClient}
+import com.whisk.docker._
+import com.whisk.docker.impl.spotify.SpotifyDockerFactory
+import com.whisk.docker.scalatest._
+import journal.Logger
+import org.http4s._
+import org.http4s.client.blaze._
+import org.scalacheck._
+import org.scalatest._
+import org.scalatest.enablers.CheckerAsserting
+import org.scalatest.prop._
 
-// trait DockerConsulService extends DockerKit {
-//   private[this] val logger = Logger[DockerConsulService]
+trait DockerConsulService extends DockerKit {
+  private[this] val logger = Logger[DockerConsulService]
 
-//   private val client: DockerClient = DefaultDockerClient.fromEnv().build()
-//   override implicit val dockerFactory: DockerFactory = new SpotifyDockerFactory(client)
+  private val client: DockerClient = DefaultDockerClient.fromEnv().build()
+  override implicit val dockerFactory: DockerFactory = new SpotifyDockerFactory(client)
 
-//   val ConsulPort = 18512
+  val ConsulPort = 18512
 
-//   val consulContainer =
-//     DockerContainer("consul:0.7.0", name = Some("consul"))
-//       .withPorts(8500 -> Some(ConsulPort))
-//       .withLogLineReceiver(LogLineReceiver(true, s => logger.debug(s"consul: $s")))
-//       .withCommand("agent", "-dev", "-client", "0.0.0.0")
-//       .withReadyChecker(DockerReadyChecker
-//         .HttpResponseCode(8500, "/v1/status/leader")
-//         .looped(12, 10.seconds))
+  val consulContainer =
+    DockerContainer("consul:0.7.0", name = Some("consul"))
+      .withPorts(8500 -> Some(ConsulPort))
+      .withLogLineReceiver(LogLineReceiver(true, s => logger.debug(s"consul: $s")))
+      .withCommand("agent", "-dev", "-client", "0.0.0.0")
+      .withReadyChecker(DockerReadyChecker
+        .HttpResponseCode(8500, "/v1/status/leader")
+        .looped(12, 10.seconds))
 
-//   abstract override def dockerContainers: List[DockerContainer] =
-//     consulContainer :: super.dockerContainers
-// }
+  abstract override def dockerContainers: List[DockerContainer] =
+    consulContainer :: super.dockerContainers
+}
 
-// class IntegrationSpec
-//     extends FlatSpec
-//     with Matchers
-//     with Checkers
-//     with BeforeAndAfterAll
-//     with DockerConsulService with DockerTestKit {
+class IntegrationSpec
+    extends FlatSpec
+    with Matchers
+    with Checkers
+    with BeforeAndAfterAll
+    with DockerConsulService with DockerTestKit {
 
-//   val client = Http1Client[IO]().unsafeRunSync
+  val client = Http1Client[IO]().unsafeRunSync
 
-//   val baseUrl: Uri =
-//     Uri.fromString(s"http://${dockerExecutor.host}:${ConsulPort}").valueOr(throw _)
+  val baseUrl: Uri =
+    Uri.fromString(s"http://${dockerExecutor.host}:${ConsulPort}").valueOr(throw _)
 
-//   val interpreter = new Http4sConsulClient(baseUrl, client)
+  val interpreter = new Http4sConsulClient(baseUrl, client)
 
-//   "consul" should "work" in check { (k: String, v: Array[Byte]) =>
-//     helm.run(interpreter, ConsulOp.kvSet(k, v)).unsafeRunSync
-//     // Equality comparison for Option[Array[Byte]] doesn't work properly. Since we expect the value to always be Some making a custom matcher doesn't seem worthwhile, so call .get on the Option
-//     // See https://github.com/scalatest/scalatest/issues/491
-//     helm.run(interpreter, ConsulOp.kvGetRaw(k, None, None)).unsafeRunSync.value.get should be (v)
+  "consul" should "work" in check { (k: String, v: Array[Byte]) =>
+    helm.run(interpreter, ConsulOp.kvSet(k, v)).unsafeRunSync
+    // Equality comparison for Option[Array[Byte]] doesn't work properly. Since we expect the value to always be Some making a custom matcher doesn't seem worthwhile, so call .get on the Option
+    // See https://github.com/scalatest/scalatest/issues/491
+    helm.run(interpreter, ConsulOp.kvGetRaw(k, None, None)).unsafeRunSync.value.get should be (v)
 
-//     helm.run(interpreter, ConsulOp.kvListKeys("")).unsafeRunSync should contain (k)
-//     helm.run(interpreter, ConsulOp.kvDelete(k)).unsafeRunSync
-//     helm.run(interpreter, ConsulOp.kvListKeys("")).unsafeRunSync should not contain (k)
-//     true
-//   }(implicitly, implicitly, Arbitrary(Gen.alphaStr suchThat(_.nonEmpty)), implicitly, implicitly, Arbitrary(Gen.nonEmptyContainerOf[Array, Byte](Gen.chooseNum(Byte.MinValue, Byte.MaxValue))), implicitly, implicitly, implicitly[CheckerAsserting[EntityDecoder[IO, Array[Byte]]]], implicitly, implicitly)
-// }
+    helm.run(interpreter, ConsulOp.kvListKeys("")).unsafeRunSync should contain (k)
+    helm.run(interpreter, ConsulOp.kvDelete(k)).unsafeRunSync
+    helm.run(interpreter, ConsulOp.kvListKeys("")).unsafeRunSync should not contain (k)
+    true
+  }(implicitly, implicitly, Arbitrary(Gen.alphaStr suchThat(_.nonEmpty)), implicitly, implicitly, Arbitrary(Gen.nonEmptyContainerOf[Array, Byte](Gen.chooseNum(Byte.MinValue, Byte.MaxValue))), implicitly, implicitly, implicitly[CheckerAsserting[EntityDecoder[IO, Array[Byte]]]], implicitly, implicitly)
+}
